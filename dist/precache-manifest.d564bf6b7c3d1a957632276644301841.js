c511cdc6014d3f9e0be6083ba78adfb76cb874ac
Joshua Ignacio <joshua.ignacio@dynameyes.com>
2020-08-07 21:54:25 +0800

    build

---
+++ dist/precache-manifest.d564bf6b7c3d1a957632276644301841.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f53a60e3e7ae1e6786b38c4206774628",
+    "url": "/_redirects"
+  },
+  {
+    "revision": "0eada3ff07eaf474e3a3",
+    "url": "/css/app.0fcf3556.css"
+  },
+  {
+    "revision": "c3e0d529d0e05d0cae64",
+    "url": "/css/chunk-vendors.5116e7f7.css"
+  },
+  {
+    "revision": "55e0d608e0fa4231f5584425e10efed5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0eada3ff07eaf474e3a3",
+    "url": "/js/app.e16f8a0f.js"
+  },
+  {
+    "revision": "c3e0d529d0e05d0cae64",
+    "url": "/js/chunk-vendors.f260cb29.js"
+  },
+  {
+    "revision": "25419a540b05d842af404542efc1abcf",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "1e9850ec094f982c9303a112c811aab4",
+    "url": "/site.webmanifest"
+  }
+]);
