5eb520db11cdf709d6376c5c7867737801307e52
Joshua Ignacio <joshua.ignacio@dynameyes.com>
2020-08-10 20:27:56 +0800

    deploy

---
+++ precache-manifest.bd22df86dfdc4900885237b9a9f1c03f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f53a60e3e7ae1e6786b38c4206774628",
+    "url": "/G3o.link/_redirects"
+  },
+  {
+    "revision": "4d50c9f7c521a91247bb",
+    "url": "/G3o.link/css/app.3c6891c5.css"
+  },
+  {
+    "revision": "4e379792b552e940675b",
+    "url": "/G3o.link/css/chunk-vendors.5116e7f7.css"
+  },
+  {
+    "revision": "87988be9103e325a2ae6ee10f35ba458",
+    "url": "/G3o.link/index.html"
+  },
+  {
+    "revision": "4d50c9f7c521a91247bb",
+    "url": "/G3o.link/js/app.92b4b753.js"
+  },
+  {
+    "revision": "4e379792b552e940675b",
+    "url": "/G3o.link/js/chunk-vendors.5895aa2e.js"
+  },
+  {
+    "revision": "25419a540b05d842af404542efc1abcf",
+    "url": "/G3o.link/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/G3o.link/robots.txt"
+  },
+  {
+    "revision": "1e9850ec094f982c9303a112c811aab4",
+    "url": "/G3o.link/site.webmanifest"
+  }
+]);
